97a2ad1edf27c55cbbe07cec949cf907e55f9e9b
Tianying <tianying@192-168-1-110.tpgi.com.au>
2024-10-20 23:08:56 +1100

    added some comments and configured the eslint

--- server/index.js
+++ server/index.js
@@ -3,8 +3,11 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import dotenv from 'dotenv';
+dotenv.config();
 import postRoutes from './routes/posts.js';
+import process from 'process';
+
 
 const app = express();
 
@@ -16,6 +19,7 @@ app.use('/posts', postRoutes);
 
 const CONNECTION_URL = `mongodb+srv://tianying123:${process.env.MONGO_DB_PASSWORD}@cluster0.eifdr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const PORT = process.env.PORT|| 5000;
+
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
